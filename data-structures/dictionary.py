ddfd959da81555032bf23411cf8da0b1fbd8108c
Datorium <45357320+datorium@users.noreply.github.com>
2023-11-15 09:34:57 +0200

    Add files via upload

---
+++ data-structures/dictionary.py
@@ -0,0 +1,27 @@
+# Skolēni, katrs skolēns pieder kādai klasei, un katram skolēnam ir
+# vairākas atzīmes, katra atzīme ir par kādu priekšmetu
+
+students = ["Anna", "Oskars"]
+# Anna (10b): 8 (Matemātika), 5 (Fizika), 9 (Bioloģija), 7 (Ķīmija), 10 (Programmēšana)
+# Oskars (10c): 9 (Matemātika), 6 (Fizika), 8 (Programmēšana)
+
+# DICTIONARY (VĀRDNĪCA)
+
+words = {
+    "cat": "kaķis",
+    "table": "galds",
+    "computer": "dators"
+}
+
+dictionary = {
+    "kaķis": "dzīvnieks ar 4 kājām un ūsam",
+    "dators": "eletroniska ierice, ..."
+}
+
+grades = {
+    "Fizika": 9, 
+    "Matematika": 8, 
+    "Bioloģija": 7
+}
+
+print(grades["Bioloģija"])
