f8ab724c80e6add77b05d8c32d54291eb4219a30
Datorium <45357320+datorium@users.noreply.github.com>
2023-11-15 10:52:35 +0200

    Add files via upload

--- data-structures/dictionary.py
+++ data-structures/dictionary.py
@@ -24,4 +24,49 @@ grades = {
     "Bioloģija": 7
 }
 
-print(grades["Bioloģija"])
+# ATRISINĀJUMS PARASTAIS
+school = {
+    "Anna": {
+        "Matemātika": 9,
+        "Fizika": 10,
+        "Bioloģija": 6,
+        "Programmēšana": 8
+    },
+    "Oskars": {
+        "Matemātika": 6,
+        "Fizika": 8,
+        "Bioloģija": 10
+    } 
+}
+
+# print(f"Skolēna atzīme: {school['Anna']['Programmēšana']}")
+
+
+# ATRISINĀJUMS PILNS
+students = [
+    {
+        "name": "Anna",
+        "class": "10b",
+        "grades": {
+            "Matemātika": 9,
+            "Fizika": 10,
+            "Bioloģija": 6,
+            "Programmēšana": 8
+        }
+    },
+    {
+        "name": "Oskars",
+        "class": "10c",
+        "grades": {
+            "Matemātika": 10,
+            "Fizika": 6,
+            "Bioloģija": 8        
+        }
+    }
+]
+
+for student in students:
+    print(f"{student['name']} no {student['class']}")
+    for subject, grade in student["grades"].items():
+        print(f"{subject}: {grade}")
+    print("----------------------")
