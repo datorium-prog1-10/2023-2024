bdf5ed8989686cce4f910edc82dcf378c781ea5f
Datorium <45357320+datorium@users.noreply.github.com>
2023-11-22 10:34:14 +0200

    Delete data-structures directory

--- data-structures/set.py
+++
@@ -1,8 +0,0 @@
-names = ['Anna', 'Oskars', 'Jennifer', 'Anna']
-names_set = set(names)
-
-shopping_list = {'lemon', 'bread', 'avocado', 'lemon'}
-
-print(names)
-print(names_set)
-print(shopping_list)
--- data-structures/tuple.py
+++
@@ -1,5 +0,0 @@
-names_list = ['Anna', 'Oskars', 'Jennifer', 'Anna']
-names_tuple = ('Anna', 'Oskars', 'Jennifer', 'Anna')
-
-names_list.remove('Anna')
-print(names_list)
