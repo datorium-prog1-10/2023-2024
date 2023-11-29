e4b7abcdad3e0fe6fd61aa6e39b8ad39d4ac2bf4
Datorium <45357320+datorium@users.noreply.github.com>
2023-11-29 10:33:01 +0200

    Add files via upload

--- Phone book/main.py
+++ Phone book/main.py
@@ -1,7 +1,7 @@
 phonebook = []
 
 while True:
-    choice = input("1-pievienot, 2-izvadīt visus ierakstus, 3-izdzēst, 4-iziet: ")
+    choice = input("1-pievienot, 2-izvadīt, 3-atrast, 4-izdzēst, 5-iziet: ")
 
     if choice == "1":
         # UZDEVUMS 1
@@ -20,12 +20,23 @@ while True:
         # UZDEVUMS 2
         # Izvadīt visus datus
         for person in phonebook:
-            print(f"{person['name']}: {person['number']}")
+            print(f"{person['name']}: {person['number']}")    
         
     elif choice == "3":
+        name = input("Kuru kontaktu gribi atrast? ")
+        found = False
+        for person in phonebook:
+            if person['name'] == name:
+                print(f"Found {person['name']}: {person['number']}")
+                found = True
+                break
+        if not found:
+            print(f"Kontaktu {name} nevaru atrast!")
+    elif choice == "4":
         #izdzēst
         pass
-    elif choice == "4":
+    elif choice == "5":
+        print("Bye, thanks for using our phonebook!")
         exit()
     else:
         print("Nebija pareiza izvēle")
--- Phone book/specs.md
+++ Phone book/specs.md
@@ -4,7 +4,8 @@ Mūsu lietotne palīdzēs saglabāt mūsu kontaktus.
 
 ### Specifikācijas
 * [] jābūt izvēlnei (menu) ar sekojošām izvēlēm:
-    * [] Pievienot kontaktu
+    * [x] Pievienot kontaktu
+    * [x] Izvadīt viss kontaktus
     * [] Atrast kontaktu
     * [] Izdzēst kontaktu
-    * [] Iziet
+    * [x] Iziet
